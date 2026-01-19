656009b1cfbc70d6a09fcdb4daa96ada887f366c
Matthew Trevino <mtrevino1983@gmail.com>
2026-01-18 22:10:12 -0500

    Fix build output directory for Render

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
